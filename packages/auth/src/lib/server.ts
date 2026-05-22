6ac84e0146914fc7a743e0836a897bac7fae3a74
HugoDemont <hugodemont62@hotmail.fr>
2026-05-22 15:18:51 +0200

    feat: add vercel configuration for build command using bunx and webpack

--- packages/auth/src/lib/server.ts
+++ packages/auth/src/lib/server.ts
@@ -8,7 +8,7 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL!,
 
   database: prismaAdapter(db, {
-    provider: "sqlite",
+    provider: "postgresql",
   }),
 
   trustedOrigins: [process.env.WEBAPP_URL!],
