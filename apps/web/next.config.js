9627cb4b1ccfc797bf483179b75e59eda8b64877
HugoDemont <hugodemont62@hotmail.fr>
2026-05-29 09:56:59 +0200

    feat: remove .mjs extension alias from webpack configuration

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
   webpack(config) {
     config.resolve.extensionAlias = {
       ".js": [".ts", ".tsx", ".js", ".jsx"],
-      ".mjs": [".mts", ".mjs"],
     };
     return config;
   },
